91207e7b3832bc9dba97ffef8be3d267204dbe1e
Panicz <godek.maciek@gmail.com>
2023-01-27 08:18:24 +0100

    Proper support for ctrl-c and ctrl-z in the terminal client

--- src/term.scm
+++ src/term.scm
@@ -44,6 +44,9 @@
 
 (define-alias TextStyle java.util.EnumSet[TextDecoration])
 
+(define-alias CTRL+C-behaviour
+  com.googlecode.lanterna.terminal.ansi.UnixLikeTerminal:CtrlCBehaviour)
+
 (define MouseButton:None ::int 0)
 (define MouseButton:Left ::int 1)
 (define MouseButton:Middle ::int 2)
@@ -55,10 +58,15 @@
   (DefaultTerminalFactory))
 
 (define (make-terminal
-	 #!key (mouse-capture-mode::MouseCaptureMode
-		MouseCaptureMode:CLICK_RELEASE_DRAG))
+	 #!key
+	 (mouse-capture-mode::MouseCaptureMode
+	  MouseCaptureMode:CLICK_RELEASE_DRAG)
+	 (ctrl+c-behaviour::CTRL+C-behaviour
+	  CTRL+C-behaviour:TRAP))
   ::Terminal
   (defaultTerminalFactory:setMouseCaptureMode mouse-capture-mode)
+  (defaultTerminalFactory:setUnixTerminalCtrlCBehaviour
+    ctrl+c-behaviour)
   (defaultTerminalFactory:createTerminal))
 
 (define (make-terminal-screen)::LanternaScreen
