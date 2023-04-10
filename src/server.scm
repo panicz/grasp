98dc58dd105ba5b880358557abd8575e5a6b0c66
panicz <godek.maciek@gmail.com>
2023-04-10 18:52:46 +0200

    implemented tcp output server

---
+++ src/server.scm
@@ -0,0 +1,20 @@
+(import (while))
+(import (for))
+
+(define (tcp-output-server port::short)::gnu.kawa.io.OutPort
+  (let* ((server ::java.net.ServerSocket
+		 (java.net.ServerSocket port))
+	 (connections ::list '())
+	 (thread (future
+		  (while #t
+		    (let ((socket ::java.net.Socket (server:accept)))
+		      (set! connections
+			    (cons (socket:getOutputStream)
+				  connections)))))))
+    (gnu.kawa.io.OutPort
+     (object (java.io.OutputStream)
+       ((write byte::int)::void
+	(for connection::java.io.OutputStream in connections
+	  (connection:write byte)
+	  (when (= byte (char->integer #\newline))
+	    (connection:flush))))))))
