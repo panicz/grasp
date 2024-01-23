c203f57ad0a66c9c7613659ba3bac175a07d9d27
panicz <godek.maciek@gmail.com>
2024-01-23 20:34:29 +0100

    improved shell integration (still mostly useless on Windows tho)

--- src/utils/shell.scm
+++ src/utils/shell.scm
@@ -1,19 +1,17 @@
 (module-name (utils shell))
 
-(define-alias InputStream java.io.InputStream)
-(define-alias Runtime java.lang.Runtime)
-(define-alias Process java.lang.Process)
 (define-alias Charset java.nio.charset.StandardCharsets)
 
 ;(define-alias PtyProcess com.pty4j.PtyProcess)
 
-(define runtime ::Runtime (Runtime:getRuntime))
+(define runtime ::java.lang.Runtime
+  (java.lang.Runtime:getRuntime))
 
 (define (shell . command-parts)::string
   (let* ((command ::string (apply string-append command-parts))
 	 (process ::Process (runtime:exec command)))
     (process:waitFor)
-    (let* ((output ::InputStream (process:getInputStream))
+    (let* ((output ::java.io.InputStream (process:getInputStream))
 	   (bytes ::int (output:available))
 	   (data ::($bracket-apply$ byte)
 		 (($bracket-apply$ byte) length: bytes)))
