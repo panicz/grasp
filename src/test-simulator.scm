b976c6d515b42a29095230209c7dac32408f3ca5
panicz <godek.maciek@gmail.com>
2025-02-08 08:10:59 +0100

    Adding a stub for simulator tests

---
+++ src/test-simulator.scm
@@ -0,0 +1,9 @@
+
+(import (language define-type))
+(import (language define-interface))
+(import (language define-object))
+(import (language infix))
+(import (language match))
+(import (language examples))
+(import (utils functions))
+(import (extra collisions))
